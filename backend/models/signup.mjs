7cb541d46a12d1b82cb27f6e49bc83d44a84b320
yamerS <y.s.aldin@gmail.com>
2023-12-19 12:50:21 +0200

    zimer

---
+++ backend/models/signup.mjs
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user"
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("userData", userSchema);
